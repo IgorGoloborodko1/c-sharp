7bf1df31a24cbca022b06b061ad8e8069e55e4d2
Igor Goloborodko <igor.goloborodko19@gmail.com>
2019-06-25 09:46:56 +0300

    add 01-task

---
+++ 01-task/FirstSln/ConsolPrj1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolPrj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Surname: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("You are {0} {1}", name, surname);
+            Console.WriteLine("It's a ConsolPrj1");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01-task/SecondSln/ConsolPrj2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibPrj3;
+
+namespace ConsolPrj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 6;
+            Console.WriteLine(Arithmetic.Add(a, b));
+            Console.WriteLine(Arithmetic.Subtract(a, b));
+            Console.WriteLine(Arithmetic.Multiply(a, b));
+            Console.WriteLine(Arithmetic.Divide(a, b));
+            Console.ReadLine();
+        }
+    }
+}
