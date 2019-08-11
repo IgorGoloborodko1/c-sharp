269334edb4be80a023e311df40e1423c44a732fb
Igor <igor.goloborodko19@gmail.com>
2019-08-11 15:21:03 +0300

    add homework-6

---
+++ homework-6/Homework-6/Homework-6/Matrix.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    public static class Matrix
+    {
+        public static int[,] AddMatrices(int[,] firstMatrix, int[,] secondMatrix)
+        {
+            if ((firstMatrix.GetUpperBound(0) + 1) != (secondMatrix.GetUpperBound(0) + 1)) throw new Exception("Matrices first dimensions are not equal!");
+            if ((firstMatrix.GetUpperBound(1) + 1) != (secondMatrix.GetUpperBound(1) + 1)) throw new Exception("Matrices second dimensions are not equal!");
+            int[,] resultMatrix = new int[firstMatrix.GetUpperBound(0) + 1, firstMatrix.GetUpperBound(1) + 1];
+            try
+            {
+                for (int i = 0; i < firstMatrix.GetUpperBound(0) + 1; i++)
+                {
+                    for (int j = 0; j < firstMatrix.GetUpperBound(1) + 1; j++)
+                    {
+                        resultMatrix[i, j] = firstMatrix[i, j] + secondMatrix[i, j];
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+            return resultMatrix;
+        }
+
+        public static int[,] SubstractMatrices(int[,] firstMatrix, int[,] secondMatrix)
+        {
+            if ((firstMatrix.GetUpperBound(0) + 1) != (secondMatrix.GetUpperBound(0) + 1)) throw new Exception("Matrices first dimensions are not equal!");
+            if ((firstMatrix.GetUpperBound(1) + 1) != (secondMatrix.GetUpperBound(1) + 1)) throw new Exception("Matrices second dimensions are not equal!");
+            int[,] resultMatrix = new int[firstMatrix.GetUpperBound(0) + 1, firstMatrix.GetUpperBound(1) + 1];
+            try
+            {
+                for (int i = 0; i < firstMatrix.GetUpperBound(0) + 1; i++)
+                {
+                    for (int j = 0; j < firstMatrix.GetUpperBound(1) + 1; j++)
+                    {
+                        resultMatrix[i, j] = firstMatrix[i, j] - secondMatrix[i, j];
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+            return resultMatrix;
+        }
+
+        public static int[,] MultiplyMatrices(int[,] firstMatrix, int[,] secondMatrix)
+        {
+            if ((firstMatrix.GetUpperBound(0) + 1) != (secondMatrix.GetUpperBound(0) + 1)) throw new Exception("Matrices first dimensions are not equal!");
+            if ((firstMatrix.GetUpperBound(1) + 1) != (secondMatrix.GetUpperBound(1) + 1)) throw new Exception("Matrices second dimensions are not equal!");
+            int[,] resultMatrix = new int[firstMatrix.GetUpperBound(0) + 1, firstMatrix.GetUpperBound(1) + 1];
+            try
+            {
+                for (int i = 0; i < firstMatrix.GetUpperBound(0) + 1; i++)
+                {
+                    for (int j = 0; j < firstMatrix.GetUpperBound(1) + 1; j++)
+                    {
+                        resultMatrix[i, j] = firstMatrix[i, j] * secondMatrix[i, j];
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+            return resultMatrix;
+        }
+    }
+}
---
+++ homework-6/Homework-6/Homework-6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = { { 2, 3, 4 }, { 1, 2, 10 }, { 3, 2, 5 } };
+            int[,] matrix2 = { { 2, 3, 4 }, { 1, 2, 5 } };
+            int[,] matrix3 = { { 2, 3, 4 }, { 1, 2, 3} };
+
+            int [,] resMatrix = Matrix.MultiplyMatrices(matrix2, matrix3);
+
+            for (int i = 0; i < resMatrix.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < resMatrix.GetUpperBound(1) + 1; j++)
+                {
+                    Console.WriteLine(resMatrix[i, j]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
